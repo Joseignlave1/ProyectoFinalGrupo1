f34aa51f911f8c4d15e742cea36fdf72c1c0377b
Jose Ignacio Lavecchia Suarez <127800259+Joseignlave1@users.noreply.github.com>
2024-11-26 22:09:31 -0300

    Finished Scroll Card Profile Image

--- src/Components/ScrollCard/ScrollCard.jsx
+++ src/Components/ScrollCard/ScrollCard.jsx
@@ -4,7 +4,7 @@ import "./scrollCard.css";
 const ScrollCard = ({ user, onClick }) => {
   return (
     <div className="card-scroll">
-      <img src={user.profilePicture || "https://i.pinimg.com/736x/79/8f/bf/798fbf62ba74a844ceeef90b83c76e59.jpg"} alt="perfil" />
+      <img src={ `http://localhost:3001${user.profilePicture}` || "https://i.pinimg.com/736x/79/8f/bf/798fbf62ba74a844ceeef90b83c76e59.jpg"} alt="perfil" />
       <div className="card-info">
         <h3>{user.username}</h3>
         <button className="viewButton" onClick={onClick}>View</button>
